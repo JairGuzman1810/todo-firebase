753bb3528bd189113a3398c75a14ba583bb05b89
Jair1810 <jair18101@hotmail.com>
2023-12-31 10:09:00 -0600

    first commit
    Agregado firebase
    Agregado inicial de lista
    Agregado inicial de detalle

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import List from './src/views/List';
+import Details from './src/views/Details';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='My Todos' component={List}></Stack.Screen>
+        <Stack.Screen name='Details' component={Details}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebaseConfig.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBNL2XZ16CcbfNyZ1Eem-E5E0vKYLecxIE",
+  authDomain: "todoapp-5f968.firebaseapp.com",
+  projectId: "todoapp-5f968",
+  storageBucket: "todoapp-5f968.appspot.com",
+  messagingSenderId: "351323553262",
+  appId: "1:351323553262:web:ecf4d2aba37691263d62ff",
+  measurementId: "G-W17DRCEPJ6",
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
